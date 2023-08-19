4ec5ee06191b42c91d90f423fce7e0d3c4f0cb99
MykeLora <maycollora16@gmail.com>
2023-08-19 15:40:04 -0600

    Hice una ruta con parametros

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ def read_root():
 @App.get("/hola")
 def Presentandome():
     return {'Desarrollador': 'Maycol Lora'}
+
+@App.get('/items/{item_id}')
+def read_item(item_id: int, q:Union[str,None]= None):
+    return {'item_id': item_id,'q':q}
