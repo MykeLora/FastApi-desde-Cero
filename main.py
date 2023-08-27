05268e7c4b16b4cc15f081b1d7093ae4e1f02abb
MykeLora <maycollora16@gmail.com>
2023-08-26 22:47:47 -0600

    Realizando unos cambios en funciones ya creadas

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ App = FastAPI()
 @App.get("/")
 
 def read_root():
-    return {'Hello': 'World!'}
+    return {'Hello': 'World!!!'}
 
 @App.get("/hola")
 def Presentandome():
-    return {'Desarrollador': 'Maycol Lora'}
+    return {'Desarrollador': 'Maycol Lora!!'}
 
 @App.get('/items/{item_id}')
 def read_item(item_id: int, q:Union[str,None]= None):
