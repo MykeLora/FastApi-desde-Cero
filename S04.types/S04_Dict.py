f069f5dd30b5ec2f2eb2f7eaa8f2cd6d182b428c
MykeLora <maycollora16@gmail.com>
2023-09-25 21:31:43 -0600

    Comprobando funcion que retorna el (dict)

--- S04.types/S04_Dict.py
+++ S04.types/S04_Dict.py
@@ -1,4 +1,5 @@
 def process_items(prices: dict[str,float]):
     for item_name,item_price in prices.items():
         print(item_name,item_price)
-        
+
+process_items({'Mause gaming': 2500.50})            
