4bb2f2ecc5ad1fa740d0a2f2d3c8a3d071eb610e
MykeLora <maycollora16@gmail.com>
2023-09-26 18:50:20 -0600

    Prueba exitosa del codigo utilizando distintos Mdl

--- S04.types/S04_pydantic.py
+++ S04.types/S04_pydantic.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 from pydantic import BaseModel
 
-
+ 
 class User(BaseModel):
-    id = int 
-    name = 'Maycol Lora Caceres'
+    id : int 
+    name : str = 'Maycol Lora Caceres'
     signup_ts : datetime | None = None
     friends: list[int] = []
 
 #Desde el exterior podriamos recibir estos datos
 external_data = {
     'id': 1002,
-    'signup_ts': '2023-9-26 14:10',
+    'signup_ts': '2023-09-26 14:10',
     'friends': [1003,10004,1005]
 }    
 user = User(**external_data)
