4159bcde1eb59c4a04533fcbfa122a2e88465d99
MykeLora <maycollora16@gmail.com>
2023-09-27 20:40:59 -0600

    Clase para representar un producto

--- Practicas_Api/Api_productos.py
+++ Practicas_Api/Api_productos.py
@@ -1,7 +1,18 @@
+from typing import Optional
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+class productos(BaseModel):
+    id : Optional[str]
+    nombre : str
+    precio_compra : float
+    precio_venta : float
+    proveedor : str
 
 app = FastAPI()
+
+productos = []
 
 @app.get('/')
 def index():
