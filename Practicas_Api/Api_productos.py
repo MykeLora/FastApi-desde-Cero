5a1f9ba5333e151786b8ded9609799cce80e201d
MykeLora <maycollora16@gmail.com>
2023-09-30 23:43:40 -0600

    Funcion para crear un producto

--- Practicas_Api/Api_productos.py
+++ Practicas_Api/Api_productos.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 from pydantic import BaseModel
 
-class productos(BaseModel):
+class producto(BaseModel):
     id : Optional[str]
     nombre : str
     precio_compra : float
@@ -19,3 +19,11 @@ def index():
     return{'Mensaje': 'Bienvenidos a la API de productos'}
 
 
+@app.get('/producto')
+def obtener_productos():
+    return productos
+
+@app.post('/producto')
+def crear_producto(producto : producto):
+    productos.append(producto)
+    return {'Mensaje':'Prodcuto creado satifactoriamente.'}
