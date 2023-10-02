9864322ab67a82215a1b96c575d97b8d7d9b76b3
MykeLora <maycollora16@gmail.com>
2023-10-02 16:14:12 -0600

    Arreglo de uuid

--- Practicas_Api/Api_productos.py
+++ Practicas_Api/Api_productos.py
@@ -1,7 +1,7 @@
 from typing import Optional
-from uuid import uuid4 as uuid
+from uuid import uuid4 as uuid 
 
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 
 class producto(BaseModel):
@@ -26,15 +26,22 @@ def obtener_productos():
 
 @app.post('/producto')
 def crear_producto(producto : producto):
-    producto.id(str(uuid))
+    producto_id = str(uuid())
+    producto.id = producto_id
     productos.append(producto)
     return {'Mensaje':'Prodcuto creado satifactoriamente.'}
 
 
+
+
 @app.get('/producto/{id_producto}')
 def obtener_producto_id(producto_id: str):
-    for p in productos:
-        if p.id == producto_id:
-            return p
+    respuesta = list(filter(lambda p:p.id == producto_id,productos))
+    
 
-    return {'Mensaje' : f'El producto con el ID {producto_id} no existe.'}        
+    if len(respuesta):
+        return respuesta[0]
+
+    raise HTTPException(status_code=404,detail=f'El producto con el ID {producto_id} no existe.')
+
+        
